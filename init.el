38ea216ca7609febda576d07b066d81fd31ddba1
jpepetrueno <jpernia15@gmail.com>
2024-11-30 19:31:45 -0400

    Feat: a minimal config with Magit & Smartparens

--- init.el
+++ init.el
@@ -1,43 +1,25 @@
-;;; **Appearance and Themes**
-;;; Load Wombat theme to customize Emacs appearance
-(load-theme 'wombat t)
+;;; init.el --- david dimagid's init.el -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 David Dimagid
+;;
+;; Author: David Dimagid  <dimagidve@gmail.com>
+;; Created: 29 Nov 2024
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; **History and Record**
-;;; Enable command history mode to save and recall previously executed commands
-(savehist-mode 1)
-;;; Set command history length to 50 entries
-(setq history-length 50)
+(load (locate-user-emacs-file "README.el"))
 
-;;; **Files and Buffers**
-;;; Enable recent files mode for easy access to recently opened files
-(recentf-mode 1)
-;;; Enable save place mode to remember cursor position in open files
-(save-place-mode 1)
-;;; Enable global auto-revert mode to automatically update buffers
-;;; when underlying files change
-(global-auto-revert-mode 1)
-;;; Revert non-file buffers as well
-(setq global-auto-revert-non-file-buffers t)
-
-;;; **Configuration and Personalization**
-;;; Set custom variables file and load it into Emacs
-(setq custom-file (locate-user-emacs-file "custom-vars.el"))
-(load custom-file 'noerror 'nomessage)
-
-;;; **Packages and Extensions**
-;;; Load use-package library to efficiently manage Emacs packages
-(require 'use-package)
-;;; Install and configure Magit package for a more user-friendly Git interface
-(use-package magit
-  :ensure t)
-;;; Install and configure Smartparens package to intelligently manage character pairs
-(use-package smartparens
-  :ensure smartparens  ;; install the package
-  :hook (prog-mode text-mode markdown-mode) ;; add Smartparens mode to these modes
-  :config
-  ;; load default configuration
-  (require 'smartparens-config))
-
-;;; **Other Configurations**
-;;; Enable visual bell instead of audible bell
-(setq visible-bell t)
+;; (org-babel-load-file (expand-file-name "$XDG_CONFIG_HOME/emacs/README.org"))
