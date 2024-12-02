1664603cf20da88ee2819fe2daaaaafd41fff7bc
jpepetrueno <jpernia15@gmail.com>
2024-12-02 10:48:07 -0400

    feat: adds undo-fu-session & display-line-numbers

--- custom-vars.el
+++ custom-vars.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-operandi))
+ '(custom-enabled-themes '(tsdh-dark))
  '(inhibit-startup-screen t)
- '(package-selected-packages '(keycast smartparens magit)))
+ '(package-selected-packages '(undo-fu-session keycast smartparens magit))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
